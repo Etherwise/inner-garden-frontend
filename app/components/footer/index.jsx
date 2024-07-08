fd3dbb6d5a228e2f791ad0e333d64c39c6c27bd6
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2024-07-08 21:52:32 +0530

    Stop Footer complete

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
+import FooterLogo from "@/public/footer/footer_logo.svg";
 import MailSubscribeComp from "./mail_subscribe";
+import LinkComp from "./link";
 
 // #TODO update 'strong' with 'heading' tag later
 export default function Footer({
@@ -10,21 +13,51 @@ export default function Footer({
 }) {
   return (
     <footer className="
-    grid place-items-center
+    flex flex-col items-center justify-center gap-y-12
     min-h-screen w-full text-white
-    bg-no-repeat bg-[url(/footer_bg.png)] bg-cover bg-center lg:bg-bottom">
+    bg-no-repeat bg-[url(/footer/footer_bg.png)] bg-cover bg-center lg:bg-bottom">
       <section className="
       flex flex-col items-center
       text-center
       w-full space-y-2">
-        <strong className="text-6xl font-extrabold">
+        <strong className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold">
             {heading}
         </strong>
-        <p className="text-3xl w-1/2">
+        <p className="text-xl lg:text-2xl xl:text-3xl md:w-1/2">
             {sub}
         </p>
       </section>
       <MailSubscribeComp />
+      <section>
+        <Image
+          src={FooterLogo}
+          width={300}
+          height={300}
+          alt="Picture of the author"
+        />
+      </section>
+      <section className="
+      flex justify-between items-center gap-9 w-max
+      text-xl xl:text-2xl">
+        <LinkComp text="welcome" href="/login" />
+        <LinkComp text="donate" href="/donate" />
+      </section>
+
+      <div className="flex flex-col items-center justify-center gap-y-3">
+        <section className="flex justify-between items-center gap-9
+        text-xl w-max">
+          <Link href="/academy">(inner) Academy</Link>
+          <Link href="/tv">(inner) TV</Link>
+        </section>
+        <section className="text-center">
+          <span>copyright 2024 Lightworkers of the Sphere, LLC. All rights reserved.</span>
+        </section>
+        <section className="flex justify-between items-center gap-9
+        text-xl w-max">
+          <Link href="/policies">policies</Link>
+          <Link href="/contact">contact</Link>
+        </section>
+      </div>
     </footer>
   );
 }
--- app/components/footer/mail_subscribe.jsx
+++ app/components/footer/mail_subscribe.jsx
@@ -7,20 +7,20 @@ import useClipPath from "@/app/hooks/clip_path_calculations";
 
 // #TODO update 'strong' with 'heading' tag later
 export default function MailSubscribeComp({}) {
-  const malSubscribeSecRef = useRef();
+  const mailSubscribeSecRef = useRef();
   const subscribeBtnRef = useRef();
 
   const { addClipPath } = useClipPath();
 
   useEffect(() => {
-    addClipPath(24, malSubscribeSecRef);
+    addClipPath(24, mailSubscribeSecRef);
     addClipPath(19, subscribeBtnRef);
   }, [addClipPath]);
 
   return (
-    <section ref={malSubscribeSecRef} className="
+    <section ref={mailSubscribeSecRef} className="
     flex gap-3 justify-between
-    w-1/3 py-1.5 px-2 bg-white text-black">
+    lg:w-1/3 py-1.5 px-2 bg-white text-black">
       <input type='mail' placeholder="Email" className="py-3 ml-6 w-full outline-0" />
       <button ref={subscribeBtnRef} className="bg-[#AF5CAA] text-white p-3 w-1/3 font-bold">subscribe</button>
     </section>
