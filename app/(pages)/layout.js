9ca0dff3ef2421d894e365c62bb0cc169059ff51
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2024-07-08 22:09:31 +0530

    Start header

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -1,10 +1,11 @@
 
+import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 
 export default function PageRootLayout({ children }) {
   return (
     <>
-        <header></header>
+        <Header />
         <main>
             {children}
         </main>
