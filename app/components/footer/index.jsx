ed7c626f333452fdda619675a830604d6f42ac5e
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2024-07-08 17:55:20 +0530

    Add footer heading and mail sub comp

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -1,12 +1,30 @@
 import Image from "next/image";
 
-export default function Footer() {
+import MailSubscribeComp from "./mail_subscribe";
+
+// #TODO update 'strong' with 'heading' tag later
+export default function Footer({
+  heading="subscribe to Wellgorithms  Weekly",
+  sub=`a weekly dose of Wellgorithms to warm your heart, 
+      invigorate your mind, and inspire resilience in adversity.`,
+}) {
   return (
     <footer className="
-      flex min-h-screen flex-col items-center justify-between p-24
-      text-white
-      bg-no-repeat bg-[url(/fotter_bg.png)] bg-cover bg-center lg:bg-bottom">
-      hello world
+    grid place-items-center
+    min-h-screen w-full text-white
+    bg-no-repeat bg-[url(/footer_bg.png)] bg-cover bg-center lg:bg-bottom">
+      <section className="
+      flex flex-col items-center
+      text-center
+      w-full space-y-2">
+        <strong className="text-6xl font-extrabold">
+            {heading}
+        </strong>
+        <p className="text-3xl w-1/2">
+            {sub}
+        </p>
+      </section>
+      <MailSubscribeComp />
     </footer>
   );
 }
