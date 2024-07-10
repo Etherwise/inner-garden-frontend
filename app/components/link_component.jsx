13fffb5b36374097b142e00964c050735d9abdc2
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2024-07-10 21:29:41 +0530

    Update link_component.jsx

--- app/components/link_component.jsx
+++ app/components/link_component.jsx
@@ -1,22 +1,27 @@
 "use client"
 
 import Link from 'next/link';
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import useClipBuilder from "@/app/hooks/clip_path_calculations";
 
 const LinkComp = ({text="", href="", className="", angle=0}) => {
   const elementRef = useRef();
 
+  const [loaded, setLoaded] = useState(false);
+
   const { hexagonClip } = useClipBuilder();
 
   useEffect(() => {
     hexagonClip(angle, elementRef);
+    setLoaded(true);
   }, [hexagonClip, angle]);
 
+  if (!loaded) return (<></>);
   return (
-    <Link ref={elementRef} href={href} className={`w-full
-    text-center font-bold ${className}`}>
+    <Link ref={elementRef} href={href} className={`
+      w-full
+      text-center font-bold ${className}`}>
       {text}
     </Link>
   );
