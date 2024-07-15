2487465fbab596b211643dd9c1186404552fb18e
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2024-07-15 17:32:46 +0530

    Stop gardens page complete

--- app/_components/label.jsx
+++ app/_components/label.jsx
@@ -8,21 +8,21 @@ import useClipBuilder from "@/app/_hooks/clip_path_calculations";
 import calculate from "@/app/_libs/align_calculation";
 
 
-const LabelComponent = ({ text=["", ""], className="", angle=24, align='' }) => {
+const LabelComponent = ({ text=["", ""], className="", angle=24, align=false }) => {
   const elementRef = useRef();
 
   const { hexagonClip } = useClipBuilder();
 
   useEffect(() => {
     hexagonClip(elementRef, angle);
-    calculate(elementRef, align);
+    align && calculate(elementRef, align);
   }, []);
 
   return (
     <section ref={elementRef} className={`
       flex items-center justify-center gap-1
-      text-center bg-purple-mid px-6 py-2 text-lg md:text-xl lg:text-2xl xl:text-3xl ${className}`}>
-      <TextInBrackets text={text[0]} className="w-[60%] md:w-[63%] font-bold text-white fill-yellow-dark" thin={{apply: true, color: "#AF5CAA", width: 3}} />
+      text-center bg-purple-mid px-6 py-3 text-lg md:text-xl lg:text-2xl xl:text-3xl ${className}`}>
+      <TextInBrackets text={text[0]} className="w-full font-bold text-white fill-yellow-dark" thin={{apply: true, color: "#AF5CAA", width: 3}} />
       <span className="text-yellow-dark font-bold">{text[1]}</span>
     </section>
   );
