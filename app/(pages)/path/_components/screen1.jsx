0c290995acedc1f848daabad0e1ecbf899ec78ba
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2024-07-17 00:59:17 +0530

    Refactor responsive fix

--- app/(pages)/path/_components/screen1.jsx
+++ app/(pages)/path/_components/screen1.jsx
@@ -15,8 +15,8 @@ import RectangleCard from "@/app/_components/cards/rectangle_card";
 export const Heading = () => {
   return (
     <>
-      <p className="flex items-center gap-1 fill-white font-bold">
-        <TextInBrackets text="title" className="text-yellow-dark font-bold" thin={{apply: true, width:3, color: "#8757B5"}} />
+      <p className="flex items-center justify-center flex-wrap gap-1 fill-white font-bold">
+        <TextInBrackets text="title" className="text-yellow-dark font-bold w-[3.3rem] sm:w-[45%] md:w-[49%] lg:w-[50%] xl:w-fit" thin={{apply: true, width:3, color: "#8757B5"}} />
         Path
       </p>
     </>
@@ -32,11 +32,12 @@ const Screen1 = () => {
   
   return (
     <>
-      <ScrollContainer containerClassName="" bodyClassName="flex flex-col pt-12" heading={<Heading />} >
-        <div className="flex gap-9">
-          <Banner className="w-full " shape="rect" angle={angle} innerangle={15}>
-            <LabelComponent shape="hexagon" thin={{apply:true, color: "#4F81E5"}}
-            className="absolute left-1/2 translate-x-[-50%] w-max z-10 bg-[#4F81E5]" text={["aware", "nest"]} align={"top"} />
+      <ScrollContainer headClassName="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl !p-3 md:!p-6 xl:!p-9" bodyClassName="flex flex-col pt-12" heading={<Heading />} >
+        <div className="flex flex-col md:flex-row gap-9">
+          <Banner className="w-full" shape="rect" angle={angle} innerangle={15}>
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[4rem] md:!w-[56%] xl:!w-[59%]"
+              className="absolute !py-1 left-1/2 !text-sm md:!text-lg xl:!text-xl
+              translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["aware", "title"]} align={"top"} />
           </Banner>
           <section className="w-full">
             <p className="font-semibold ">
@@ -48,30 +49,35 @@ const Screen1 = () => {
           </section>
         </div>
 
-        <div className="grid md:grid-cols-3 gap-12 mt-20">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-12 mt-20">
           <RectangleCard cardImage={CardImage} className={{
               main: "",
               image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
+              text: `text-pretty text-lg sm:text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
           }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="w-[6.6rem] md:w-[70%] lg:w-[69%] xl:w-[70%]"
+            className="absolute !py-1 left-1/2 top-0 translate-y-[-50%] !text-sm md:!text-lg xl:!text-xl
+            translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["compassion", "title"]} />
           </RectangleCard>
+
           <RectangleCard cardImage={CardImage} className={{
               main: "",
               image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
+              text: `text-pretty text-lg sm:text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
           }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="w-[6.6rem] md:w-[70%] lg:w-[69%] xl:w-[70%]"
+            className="absolute !py-1 left-1/2 top-0 translate-y-[-50%] !text-sm md:!text-lg xl:!text-xl
+            translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["compassion", "title"]} />
           </RectangleCard>
+
           <RectangleCard cardImage={CardImage} className={{
               main: "",
               image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
+              text: `text-pretty text-lg sm:text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
           }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="w-[6.6rem] md:w-[70%] lg:w-[69%] xl:w-[70%]"
+            className="absolute !py-1 left-1/2 top-0 translate-y-[-50%] !text-sm md:!text-lg xl:!text-xl
+            translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["compassion", "title"]} />
           </RectangleCard>
         </div>
       </ScrollContainer>
--- app/(pages)/path/subpage/_components/screen1.jsx
+++ app/(pages)/path/subpage/_components/screen1.jsx
@@ -14,9 +14,9 @@ import useClipBuilder from "@/app/_hooks/clip_path_calculations";
 export const Heading = () => {
   return (
     <>
-      <p className="flex items-center gap-1 fill-white font-bold">
+      <p className="flex items-center justify-center flex-wrap gap-1 fill-white font-bold">
         Journey to the
-        <TextInBrackets text="title" className="text-yellow-dark font-bold" thin={{apply: true, width:3, color: "#8757B5"}} />
+        <TextInBrackets text="title" className="text-yellow-dark font-bold w-[3.3rem] sm:w-[18%] md:w-[19%] lg:w-[20%] xl:w-fit" thin={{apply: true, width:3, color: "#8757B5"}} />
         Garden
       </p>
     </>
@@ -39,94 +39,42 @@ const Screen1 = () => {
   
   return (
     <>
-      <ScrollContainer heading={<Heading />} headClassName="pb-12" bodyClassName="flex flex-col pt-12">
-        <section id="test" className="absolute top-0 left-1/2 translate-x-[-50%] translate-y-[-50%] drop-shadow-clip-outline-purple">
-          <p ref={headingRef} className={`font-extrabold text-xl 
+      <ScrollContainer heading={<Heading />} headClassName="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl pb-12" bodyClassName="flex flex-col pt-12">
+        <section id="test" className="absolute top-0 left-1/2 translate-x-[-50%] translate-y-[-50%] drop-shadow-clip-outline-purple  w-max">
+          <p ref={headingRef} className={`font-extrabold text-sm md:text-lg xl:text-xl
           p-3 px-6 bg-white text-purple-dark`}>
             Martin Butterfly
           </p>
         </section>
 
-        <div className="grid md:grid-cols-3 gap-12 mt-10">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-12 mt-10">
           <RectangleCard cardImage={CardImage} className={{
               main: "",
               image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
+              text: `text-pretty text-lg sm:text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
           }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="w-[6.6rem] md:w-[70%] lg:w-[69%] xl:w-[70%]"
+            className="absolute !py-1 left-1/2 top-0 translate-y-[-50%] !text-sm md:!text-lg xl:!text-xl
+            translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["compassion", "title"]} />
+          </RectangleCard>
+          
+          <RectangleCard cardImage={CardImage} className={{
+              main: "",
+              image: "!h-[30%]",
+              text: `text-pretty text-lg sm:text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
+          }}>
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="w-[6.6rem] md:w-[70%] lg:w-[69%] xl:w-[70%]"
+            className="absolute !py-1 left-1/2 top-0 translate-y-[-50%] !text-sm md:!text-lg xl:!text-xl
+            translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["compassion", "title"]} />
           </RectangleCard>
           <RectangleCard cardImage={CardImage} className={{
               main: "",
               image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
+              text: `text-pretty text-lg sm:text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
           }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-        </div>
-      </ScrollContainer>
-      <ScrollContainer heading={<Heading />} containerClassName="" bodyClassName="flex flex-col pt-12">
-        <div className="grid md:grid-cols-3 gap-12 mt-10">
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-        </div>
-      </ScrollContainer>
-      <ScrollContainer heading={<Heading />} containerClassName="" bodyClassName="flex flex-col pt-12">
-        <div className="grid md:grid-cols-3 gap-12 mt-10">
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
-          </RectangleCard>
-          <RectangleCard cardImage={CardImage} className={{
-              main: "",
-              image: "!h-[30%]",
-              text: `text-pretty text-xl md:text-xl lg:text-2xl !xl:px-12 !lg:px-10 !md:px-8 !px-12 !text-black !h-fit !py-0 !m-0 !mt-[10%]`
-          }}>
-            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="!w-[70%]"
-            className="absolute !py-2 left-1/2 top-0 translate-y-[-50%] translate-x-[-50%] w-max z-10 bg-[#4F81E5] !text-xl" text={["compassion", "title"]} />
+            <LabelComponent shape={"hexagon"} thin={{apply:true, color: "#4F81E5", width: 3}} bracketClassName="w-[6.6rem] md:w-[70%] lg:w-[69%] xl:w-[70%]"
+            className="absolute !py-1 left-1/2 top-0 translate-y-[-50%] !text-sm md:!text-lg xl:!text-xl
+            translate-x-[-50%] w-max z-10 !bg-[#4F81E5]" text={["compassion", "title"]} />
           </RectangleCard>
         </div>
       </ScrollContainer>
